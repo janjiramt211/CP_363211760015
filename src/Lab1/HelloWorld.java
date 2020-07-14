b548ac4d46a61f8e3c519e55b1924cdc336e0e9a
bow <janjira.ja@rmutsvmail.com>
2020-07-14 17:54:36 +0700

    add code 2 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Lab1.HelloWorld!!!");
         System.out.println("janjira jampathong");
+        System.out.println("janjira.ja@rmutsvmail.com");
+        System.out.println("bow");
+
+
     }
 
 
