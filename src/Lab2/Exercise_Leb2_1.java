c2a58d9db15a3fd27b714f97301dfa217ef8319a
janjiramt211 <janjira.ja@rmutsvmail.com>
2020-08-04 20:48:35 +0700

    5ข้อในคาบ

--- src/Lab2/Exercise_Leb2_1.java
+++ src/Lab2/Exercise_Leb2_1.java
@@ -4,13 +4,29 @@ import java.util.Scanner;
 
 public class Exercise_Leb2_1 {
 
-    public static void main(int[] args) {
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int test1;
-        System.out.print("What is your test1?");
-        test1 = scanner.nextInt();
+        System.out.print("Enter a number 1: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter b number 2: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter c number 3: ");
+        int c = scanner.nextInt();
+        System.out.print("Enter d number 4: ");
+        int d = scanner.nextInt();
+        System.out.print("Enter f number 5: ");
+        int f = scanner.nextInt();
+
+
+        //finding
+        int total = a + b + c + d +f;
+        int avg = total / 5;
+
+
+        //display
+        System.out.println("Total = " +total);
+        System.out.println("Average = " +avg);
 
 
 
