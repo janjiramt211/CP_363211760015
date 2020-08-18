c7b48302cbd206ba060c92f5bba6b6b19d5aee10
Bowlab2 <janjira.ja@rmutsvmail.com>
2020-08-18 16:30:43 +0700

    Excercise Leb3_1-3_3

---
+++ src/Leb_3/Example_While.java
@@ -0,0 +1,24 @@
+package Leb_3;
+
+import java.util.Scanner;
+
+//รับค่าจำนวนเต็มจากผู้ใช้ เมื่อผู้ใช้ใส่ค่า 0 ให้จบการทำงาน (While)
+public class Example_While {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = 1;
+
+        while (num != 0) {
+            System.out.print("Enter integer : ");
+            num = sc.nextInt();
+        }
+        System.out.println("End. Good Bye.");
+
+
+
+
+    }
+}
